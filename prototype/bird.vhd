3261a72b4be98b66e2a50df0cf9e37e5b02bb587
Lauren Parker <lpar105@UoA.auckland.ac.nz>
2023-05-04 13:54:04 +1200

    [feat] Bird and pipe merged. Pipes cleaned up. LSFR cleaned up

---
+++ prototype/bird.vhd
@@ -0,0 +1,75 @@
+LIBRARY IEEE;
+USE IEEE.STD_LOGIC_1164.all;
+USE  IEEE.STD_LOGIC_ARITH.all;
+USE  IEEE.STD_LOGIC_SIGNED.all;
+
+
+ENTITY bird IS
+	PORT
+		( clk, vert_sync, left_click	: IN std_logic;
+          pixel_row, pixel_column	: IN std_logic_vector(9 DOWNTO 0);
+		  red, green, blue 			: OUT std_logic);		
+END bird;
+
+architecture flap of bird is
+
+SIGNAL bird_on					: std_logic;
+SIGNAL size 					: std_logic_vector(9 DOWNTO 0);  
+SIGNAL bird_y_pos				: std_logic_vector(9 DOWNTO 0);
+SiGNAL bird_x_pos				: std_logic_vector(10 DOWNTO 0);
+SIGNAL bird_y_motion			: std_logic_vector(9 DOWNTO 0);
+
+BEGIN           
+
+size <= CONV_STD_LOGIC_VECTOR(8,10);
+-- bird_x_pos and ball_y_pos show the (x,y) for the centre of bird
+bird_x_pos <= CONV_STD_LOGIC_VECTOR(320,11);
+
+bird_on <= '1' when ( ('0' & bird_x_pos <= '0' & pixel_column + size) and ('0' & pixel_column <= '0' & bird_x_pos + size) 	-- x_pos - size <= pixel_column <= x_pos + size
+					and ('0' & bird_y_pos <= pixel_row + size) and ('0' & pixel_row <= bird_y_pos + size) )  else	-- y_pos - size <= pixel_row <= y_pos + size
+			'0';
+
+
+-- Colours for pixel data on video signal
+-- Changing the background and bird
+Blue <= bird_on;
+
+
+Move_Bird: process (vert_sync) 
+variable prev_click_status: std_logic := '0';
+variable velocity: integer := 150;
+variable thrust: integer := 0;	
+begin
+	-- Move bird once every vertical sync
+	if (rising_edge(vert_sync)) then
+		-- Update thrust 
+		if (thrust <= 20) then
+			thrust := 0;
+		else
+			thrust := thrust - 10;
+		end if;
+		
+		-- Set motion
+		if(left_click = '1' and prev_click_status = '0') then
+			velocity := 150;
+			thrust := 150;
+			bird_y_motion <= - CONV_STD_LOGIC_VECTOR(0,10);
+		elsif (bird_y_pos < 470 or thrust > 0) then
+			velocity := velocity + 20 - thrust;
+			bird_y_motion <= CONV_STD_LOGIC_VECTOR(velocity/100,10);
+		else
+			bird_y_motion <= CONV_STD_LOGIC_VECTOR(0,10);
+		end if;
+		bird_y_pos <= bird_y_pos + bird_y_motion;
+		
+		-- Update previous click status
+		if(left_click = '1') then
+			prev_click_status := '1';
+		else 
+			prev_click_status := '0';
+		end if;
+	end if;
+end process Move_Bird;
+
+END flap;
+
---
+++ prototype/pipe_function.vhd
@@ -0,0 +1,60 @@
+LIBRARY IEEE;
+USE IEEE.STD_LOGIC_1164.all;
+USE  IEEE.STD_LOGIC_ARITH.all;
+USE  IEEE.STD_LOGIC_SIGNED.all;
+
+
+
+ENTITY pipe_function IS
+	PORT
+		(clk, vert_sync	: IN std_logic;
+          pixel_row, pixel_column	: IN std_logic_vector(9 DOWNTO 0);
+			random_num: 		IN std_logic_vector(6 DOWNTO 0);
+		  red, green, blue 			: OUT std_logic);		
+END pipe_function;
+
+architecture behavior of pipe_function is
+
+SIGNAL pipe_on					: std_logic;
+SIGNAL pipe_height 			: std_logic_vector(10 DOWNTO 0);  
+SIGNAL pipe_width 			: std_logic_vector(9 DOWNTO 0); 
+SIGNAL pipe_y_pos				: std_logic_vector(9 DOWNTO 0);
+SiGNAL pipe_x_pos				: std_logic_vector(10 DOWNTO 0);
+SIGNAL pipe_x_motion			: std_logic_vector(9 DOWNTO 0);
+SIGNAL pipe_gap				: std_logic_vector(10 DOWNTO 0);
+
+BEGIN           
+
+pipe_y_pos <= CONV_STD_LOGIC_VECTOR(240,10);
+pipe_height <= CONV_STD_LOGIC_VECTOR(480,11);
+pipe_width <= CONV_STD_LOGIC_VECTOR(30,10);
+
+
+pipe_on <= '1' when ( ('0' & (pipe_x_pos + 15) <= '0' & pixel_column + pipe_width) and ('0' & pixel_column <= '0' & (pipe_x_pos + 15) + pipe_width) 	-- x_pos - size <= pixel_column <= x_pos + size
+					and ('0' & pipe_y_pos <= pixel_row + pipe_height) and ('0' & pixel_row <= pipe_y_pos + pipe_height) and not(250 + pipe_gap > pixel_row AND pixel_row > 150 + pipe_gap) ) 
+					else '0';-- y_pos - size <= pixel_row <= y_pos + size
+			
+
+
+-- Colours for pixel data on video signal
+-- Changing the background and ball colour by pushbuttons
+Green <= pipe_on;
+
+
+Move_Pipe: process (vert_sync)
+begin
+	-- Move ball once every vertical sync
+	if (rising_edge(vert_sync)) then
+		if (pipe_x_pos + 30 < 0) then
+			pipe_gap <= "00000110010" + random_num;
+			pipe_x_pos <= CONV_STD_LOGIC_VECTOR(640 + 15,11);	
+			pipe_x_motion <= - CONV_STD_LOGIC_VECTOR(2,10);
+		else	
+			pipe_x_motion <= - CONV_STD_LOGIC_VECTOR(2,10);
+			pipe_x_pos <= pipe_x_pos + pipe_x_motion;
+		end if;
+	end if;
+end process Move_Pipe;
+
+END behavior;
+
