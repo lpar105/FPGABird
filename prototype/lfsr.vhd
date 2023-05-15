daf2b5a9de644931c61a35ad029b4849f3568969
Liam Gerritsen <lger522@UoA.auckland.ac.nz>
2023-05-15 16:59:54 +1200

    Two pipes on screen with reset glitch

--- prototype/lfsr.vhd
+++ prototype/lfsr.vhd
@@ -1,27 +1,47 @@
-library ieee; 
+library ieee;
 use ieee.std_logic_1164.all;
 
 entity lfsr is 
-	port (
-		clk: in  std_logic;
-		o_lsfr: out std_logic_vector (6 downto 0));
-	end lfsr;
+  port (
+    clk: in  std_logic;
+    o_lsfr1: out std_logic_vector (6 downto 0);
+    o_lsfr2: out std_logic_vector (6 downto 0)
+  );
+end lfsr;
 	
 architecture rtl of lfsr is   
 
-signal r_lfsr: std_logic_vector (7 downto 1):= "0010101";
-	begin  
-		o_lsfr  <= r_lfsr(7 downto 1);
-		p_lfsr : process (clk) begin 
-		
-		if rising_edge(clk) then 
-				r_lfsr(7) <= r_lfsr(1);
-				r_lfsr(6) <= r_lfsr(7) xor r_lfsr(1);
-				r_lfsr(5) <= r_lfsr(6);
-				r_lfsr(4) <= r_lfsr(5);
-				r_lfsr(3) <= r_lfsr(4);
-				r_lfsr(2) <= r_lfsr(3);
-				r_lfsr(1) <= r_lfsr(2);
-  end if; 
-end process p_lfsr; 
+  signal r_lfsr1: std_logic_vector (7 downto 1):= "0010101";
+  signal r_lfsr2: std_logic_vector (7 downto 1):= "0010101";
+  
+begin  
+
+  p_lfsr1 : process (clk) begin 
+    if rising_edge(clk) then 
+      r_lfsr1(7) <= r_lfsr1(1);
+      r_lfsr1(6) <= r_lfsr1(7) xor r_lfsr1(1);
+      r_lfsr1(5) <= r_lfsr1(6);
+      r_lfsr1(4) <= r_lfsr1(5);
+      r_lfsr1(3) <= r_lfsr1(4);
+      r_lfsr1(2) <= r_lfsr1(3);
+      r_lfsr1(1) <= r_lfsr1(2);
+    end if; 
+  end process p_lfsr1; 
+	
+  o_lsfr1  <= r_lfsr1(7 downto 1);
+	
+  p_lfsr2 : process (clk) begin 
+    if rising_edge(clk) then 
+      r_lfsr2(7) <= r_lfsr2(1);
+      r_lfsr2(6) <= r_lfsr2(7) xor r_lfsr2(1);
+      r_lfsr2(5) <= r_lfsr2(6);
+      r_lfsr2(4) <= r_lfsr2(5);
+      r_lfsr2(3) <= r_lfsr2(4);
+      r_lfsr2(2) <= r_lfsr2(3);
+      r_lfsr2(1) <= r_lfsr2(2);
+    end if; 
+  end process p_lfsr2; 
+	
+  o_lsfr2  <= r_lfsr2(7 downto 1);
+  
 end architecture rtl;
--- prototype/pipe_function.vhd
+++ prototype/pipe_function.vhd
@@ -9,29 +9,39 @@ ENTITY pipes IS
 	PORT
 		(clk, vert_sync, enable		: IN std_logic;
        pixel_row, pixel_column	: IN std_logic_vector(9 DOWNTO 0);
-		 random_num						: IN std_logic_vector(6 DOWNTO 0);
+		 random_num1						: IN std_logic_vector(6 DOWNTO 0);
+		 random_num2						: IN std_logic_vector(6 DOWNTO 0);
 		 red, green, blue, halfway : OUT std_logic);		
 END pipes;
 
 architecture behavior of pipes is
 
-SIGNAL pipe_on					: std_logic;
-SIGNAL pipe_height 			: std_logic_vector(10 DOWNTO 0);  
-SIGNAL pipe_width 			: std_logic_vector(9 DOWNTO 0); 
-SIGNAL pipe_y_pos				: std_logic_vector(9 DOWNTO 0);
-SiGNAL pipe_x_pos				: std_logic_vector(10 DOWNTO 0);
-SIGNAL pipe_x_motion			: std_logic_vector(9 DOWNTO 0);
-SIGNAL pipe_gap				: std_logic_vector(10 DOWNTO 0);
+SIGNAL pipe1_on					: std_logic;
+SIGNAL pipe2_on					: std_logic;
+SIGNAL pipe_height 				: std_logic_vector(10 DOWNTO 0);  
+SIGNAL pipe_width 				: std_logic_vector(9 DOWNTO 0); 
+SIGNAL pipe1_y_pos				: std_logic_vector(9 DOWNTO 0);
+SIGNAL pipe2_y_pos				: std_logic_vector(9 DOWNTO 0);
+SiGNAL pipe1_x_pos				: std_logic_vector(10 DOWNTO 0);  
+SiGNAL pipe2_x_pos				: std_logic_vector(10 DOWNTO 0);	
+SIGNAL pipe_x_motion				: std_logic_vector(9 DOWNTO 0);
+SIGNAL pipe1_gap					: std_logic_vector(10 DOWNTO 0);
+SIGNAL pipe2_gap					: std_logic_vector(10 DOWNTO 0);
 
 BEGIN           
 
-pipe_y_pos <= CONV_STD_LOGIC_VECTOR(240,10);
+pipe1_y_pos <= CONV_STD_LOGIC_VECTOR(240,10);
+pipe2_y_pos <= CONV_STD_LOGIC_VECTOR(240,10);
 pipe_height <= CONV_STD_LOGIC_VECTOR(480,11);
 pipe_width <= CONV_STD_LOGIC_VECTOR(30,10);
 
 
-pipe_on <= '1' when ( ('0' & (pipe_x_pos + 15) <= '0' & pixel_column + pipe_width) and ('0' & pixel_column <= '0' & (pipe_x_pos + 15) + pipe_width) 	-- x_pos - size <= pixel_column <= x_pos + size
-					and ('0' & pipe_y_pos <= pixel_row + pipe_height) and ('0' & pixel_row <= pipe_y_pos + pipe_height) and not(350 + pipe_gap > pixel_row AND pixel_row > 50 + pipe_gap) and (enable = '1'))
+pipe1_on <= '1' when ( ('0' & (pipe1_x_pos + 15) <= '0' & pixel_column + pipe_width) and ('0' & pixel_column <= '0' & (pipe1_x_pos + 15) + pipe_width) 	-- x_pos - size <= pixel_column <= x_pos + size
+					and ('0' & pipe1_y_pos <= pixel_row + pipe_height) and ('0' & pixel_row <= pipe1_y_pos + pipe_height) and not(350 + pipe1_gap > pixel_row AND pixel_row > 50 + pipe1_gap) and (enable = '1'))
+					else '0';-- y_pos - size <= pixel_row <= y_pos + size
+					
+pipe2_on <= '1' when ( ('0' & (pipe2_x_pos + 15) <= '0' & pixel_column + pipe_width) and ('0' & pixel_column <= '0' & (pipe2_x_pos + 15) + pipe_width) 	-- x_pos - size <= pixel_column <= x_pos + size
+					and ('0' & pipe2_y_pos <= pixel_row + pipe_height) and ('0' & pixel_row <= pipe2_y_pos + pipe_height) and not(350 + pipe2_gap > pixel_row AND pixel_row > 50 + pipe2_gap) and (enable = '1'))
 					else '0';-- y_pos - size <= pixel_row <= y_pos + size
 					
 --halfway <= '1' when (pipe_x_pos < 280 AND pipe_x_pos > 250) else '0';
@@ -40,25 +50,33 @@ pipe_on <= '1' when ( ('0' & (pipe_x_pos + 15) <= '0' & pixel_column + pipe_widt
 
 -- Colours for pixel data on video signal
 -- Changing the background and ball colour by pushbuttons
-Green <= pipe_on;
+Green <= pipe1_on or pipe2_on;
 
 
 Move_Pipe: process (vert_sync)
 begin
 	-- Move ball once every vertical sync
 	if (rising_edge(vert_sync)) then
-		if (pipe_x_pos + 30 < 0) then
-			pipe_gap <= "00000110010" + random_num;
-			pipe_x_pos <= CONV_STD_LOGIC_VECTOR(640 + 15,11);	
+		if (pipe1_x_pos < 2) then				
+			pipe2_x_pos <= CONV_STD_LOGIC_VECTOR(320,11);
+			pipe1_x_pos <= CONV_STD_LOGIC_VECTOR(640,11);
+			pipe2_gap <= "00000110010" + random_num2;	
+			pipe_x_motion <= - CONV_STD_LOGIC_VECTOR(2,10);
+			halfway <= '1';
+		end if;
+		if (pipe2_x_pos < 0) then		
+			pipe1_x_pos <= CONV_STD_LOGIC_VECTOR(320,11);
+			pipe2_x_pos <= CONV_STD_LOGIC_VECTOR(640,11);
+			pipe1_gap <= "00000110010" + random_num1;		
 			pipe_x_motion <= - CONV_STD_LOGIC_VECTOR(2,10);
 			halfway <= '1';
 		else	
 			pipe_x_motion <= - CONV_STD_LOGIC_VECTOR(2,10);
-			pipe_x_pos <= pipe_x_pos + pipe_x_motion;
+			pipe1_x_pos <= pipe1_x_pos + pipe_x_motion;
+			pipe2_x_pos <= pipe2_x_pos + pipe_x_motion;
 			halfway <= '0';
 		end if;
 	end if;
 end process Move_Pipe;
 
 END behavior;
-
