c70ed5ea1a3436db96a4c2d9101a443297c7fc14
hsal869 <100745734+hsal869@users.noreply.github.com>
2023-05-22 15:29:57 +1200

    Start Menu Implemented
    start menu working well
    
    can start game and change game modes

--- prototype/view_selector.vhd
+++ prototype/view_selector.vhd
@@ -6,7 +6,7 @@ USE  IEEE.STD_LOGIC_SIGNED.all;
 
 ENTITY view_selector IS
 	PORT
-		(red_game, green_game, blue_game, text_in: IN std_logic;
+		(red_game, green_game, blue_game, text_in, enable: IN std_logic;
 			 mode: IN std_logic_vector(2 downto 0);
 			 red, green, blue: OUT Std_logic);		
 END view_selector;
@@ -14,9 +14,23 @@ END view_selector;
 
 architecture views of view_selector is
 begin
-	red <= text_in;
-	green <= green_game;
-	blue <= blue_game;
+	
+	process(enable)
+	BEGIN
+		if (enable = '0') then -- Game in Menu Mode 
+		
+			red <= text_in;
+			green <= green_game;
+			blue <= blue_game;
+		
+		else -- Game is running
+			
+			red <= text_in;
+			green <= green_game;
+			blue <= blue_game;
+		
+		end if;
+	end process;
 	
 END views;
 
